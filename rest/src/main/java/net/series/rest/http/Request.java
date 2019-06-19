034ba7c3d3b5b6c18dda868ac42505e8a213e533
agge <boundlessvolatilecrow@gmail.com>
2019-06-19 12:49:37 +0200

    update response, to save appropriate fields

--- rest/src/main/java/net/series/rest/http/Request.java
+++ rest/src/main/java/net/series/rest/http/Request.java
@@ -1,15 +1,12 @@
 package net.series.rest.http;
 
 import com.google.gson.Gson;
-
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-
 
 @Service
 public class Request {
--- rest/src/main/java/net/series/rest/http/Response.java
+++ rest/src/main/java/net/series/rest/http/Response.java
@@ -2,13 +2,43 @@ package net.series.rest.http;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * GSON Object
  */
 @Data
 public class Response {
-    public int episode_number;
-    public int season_number;
-    public String overview;
     public int id;
+    public int number_of_seasons;
+    public String overview;
+
+    public List<Result> results;
+    public List<Season> seasons;
+    public List<Episode> episodes;
+
+    @Data
+    class Result {
+        int id;
+        String name;
+        String overview;
+        String first_air_date;
+        String original_name;
+    }
+
+    @Data
+    class Season {
+        int season_number;
+        int episode_count;
+        String name;
+        String overview;
+    }
+
+    @Data
+    class Episode {
+        int season_number;
+        int episode_number;
+        String name;
+        String overview;
+    }
 }
