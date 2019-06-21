e08c0e5e9e5d3dc610fcc7d0193f51964cc592ac
agge <boundlessvolatilecrow@gmail.com>
2019-06-21 09:21:24 +0200

    update python dockerfile and add redis (app.py)

--- web/app.py
+++ web/app.py
@@ -1,10 +1,13 @@
+import redis
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_count():
+        return cache.incr('hits')
 
 @app.route('/')
-def hello():
-    return "test"
-
-if __name__ == '__main__':
-    app.run()
+def index():
+    count = get_count()
+    return '{}\n'.format(count)
