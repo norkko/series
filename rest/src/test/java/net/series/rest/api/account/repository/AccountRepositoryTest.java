0caa884657b8889bf5313cc88de5c6b8d76b6cc3
agge <boundlessvolatilecrow@gmail.com>
2019-06-24 16:13:07 +0200

    fix app.yaml, refactor account repo tests

--- rest/src/test/java/net/series/rest/api/account/repository/AccountRepositoryTest.java
+++ rest/src/test/java/net/series/rest/api/account/repository/AccountRepositoryTest.java
@@ -1,14 +1,12 @@
-package net.series.rest.account.repository;
+package net.series.rest.api.account.repository;
 
 import net.series.rest.api.account.Account;
-import net.series.rest.api.account.repository.AccountRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -25,46 +23,40 @@ public class AccountRepositoryTest {
     @Autowired
     private AccountRepository accountRepository;
 
-    // use get stub method
-
     @Test
     public void shouldFindById() {
-        Account account = new Account();
-        account.setUsername("user");
-        account.setPassword(encoder.encode("password"));
-
+        final Account account = getAccountStub();
         accountRepository.save(account);
 
-        Account found = accountRepository.findById(account.getId());
+        final Account found = accountRepository.findById(account.getId());
         assertEquals(found, account);
     }
 
     @Test
     public void shouldFindByUsername() {
-        String username = "user";
-
-        Account account = new Account();
-        account.setUsername(username);
-        account.setPassword(encoder.encode("password"));
-
+        final Account account = getAccountStub();
         accountRepository.save(account);
 
-        Account found = accountRepository.findByUsername(username);
+        final Account found = accountRepository.findByUsername(account.getUsername());
         assertEquals(found, account);
     }
 
     @Test
     public void shouldDeleteAccount() {
-        Account account = new Account();
-        account.setUsername("user");
-        account.setPassword(encoder.encode("password"));
-        accountRepository.save(account);
+        final Account account = getAccountStub();
 
+        accountRepository.save(account);
         assertNotNull(accountRepository.findById(account.getId()));
 
         accountRepository.deleteById(account.getId());
-
         assertNull(accountRepository.findById(account.getId()));
+    }
+
+    private Account getAccountStub() {
+        Account account = new Account();
+        account.setUsername("user");
+        account.setPassword(encoder.encode("password"));
+        return account;
     }
 
 }
